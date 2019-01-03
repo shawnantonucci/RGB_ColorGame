a62df13d8d32b540e26cb53434a2fc982feae9fb
shawn <shawnantonucci2018@gmail.com>
2019-01-02 18:37:15 -0800

    added random color logic

--- colorGame.js
+++ colorGame.js
@@ -8,7 +8,7 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 
@@ -37,4 +37,9 @@ function changeColors(color) {
     for (let i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = color;
     }
+}
+
+function pickColor() {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
 }
